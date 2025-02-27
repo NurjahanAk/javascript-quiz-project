1a4082c9c33c47436cf343980d3792cef9d52fb3
Vassilis Bousbourelis <vasbousdev@gmail.com>
2025-02-27 16:09:37 +0100

    Quiz updated code

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,64 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
+  // 2. getQuestion()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  // 3. moveToNextQuestion()
+  moveToNextQuestion() {
+    this.currentQuestionIndex++;
+  }
+  // 4. shuffleQuestions()
+  shuffleQuestions() {
+    for (let i = this.questions.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.questions[i], this.questions[j]] = [
+        this.questions[j],
+        this.questions[i],
+      ];
+    }
+  }
 
-    // 4. shuffleQuestions()
+  // 5. checkAnswer(answer)
+  checkAnswer(answer) {
+    this.currentQuestion = this.questions[this.currentQuestionIndex];
+    //this.answer=answer;
+    if (answer === this.currentQuestion.answer) {
+      this.correctAnswers++;
+    }
+  }
+  // 6. hasEnded()
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    } else if ((this.currentQuestionIndex = this.questions.length)) {
+      return true;
+    }
+  }
 
-    // 5. checkAnswer(answer)
+  filterQuestionsByDifficulty(difficulty) {
+    if (![1, 2, 3].includes(difficulty)) {
+      return;
+    }
 
-    // 6. hasEnded()
+    this.questions = this.questions.filter((q) => q.difficulty === difficulty);
+  }
+
+  averageDifficulty() {
+    const totalDifficulty = this.questions.reduce(
+      (totDiff, diff) => totDiff + diff.difficulty,
+      0
+    );
+    return totalDifficulty / this.questions.length;
+  }
 }
